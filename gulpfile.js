573c56a0fcc08854f9efa99f1d95902da4422e33
yangshize <eternity.instant1@gmail.com>
2020-03-02 00:12:43 +0800

    版本1.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -86,10 +86,13 @@ const serverHandler = () => {
                proxies: [
                  // 每一个代理配置就是一个对象
                  {
-                   source: '/gx', // 源, 你的代理标识符
-                   // 你直接请求下面这个地址压根也拿不到东西, 因为跨域了
-                   target: 'http://localhost/zhuce.php' // 目标, 你要代理的地址
-                 }
+                   source: '/gx',
+                   target: 'http://localhost/zhuce.php'
+                 },
+                 {
+                  source: '/log',
+                  target: 'http://localhost/login.php' 
+                }
                ]
              })) // 开启服务器
 }
